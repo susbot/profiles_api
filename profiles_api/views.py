16407c848572c3ca23525181e39386a93ea8b889
Susbot <susbot651968498@gmail.com>
2022-05-17 18:47:00 -0600

    permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -14,7 +14,14 @@ from profiles_api import serializers
 
 """"""
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 # Create your views here.
+
+""""""
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -102,3 +109,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
